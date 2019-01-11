4e61433f518e5b7e88406e4a9d3a5b0dc37360ba
Hazza Alkaabi <hazza.r.alkaabi@gmail.com>
2019-01-11 17:50:56 +0400

    started the layout work with some simple content

---
+++ src/pages/about.js
@@ -0,0 +1,9 @@
+import React from "react"
+import Layout from "../components/layout"
+
+export default () => (
+    <Layout>
+        <h1>About me</h1>
+        <p>Computer Scientist. University of Arizona Alumni. Advisor at <a href="https://hackarizona.org/">Hack Arizona</a>. Developer. Writer.</p>
+    </Layout>
+)
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,9 @@
-import React from 'react'
-import { Link } from 'gatsby'
+import React from "react"
+import Layout from "../components/layout"
 
-import Layout from '../components/layout'
-import Image from '../components/image'
-import SEO from '../components/seo'
-
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" keywords={['gatsby', 'application', 'react']} />
-    <h1>Welcome to Majarah</h1>
-    <h2>Collection of projects &amp; writings.</h2>
-    <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+export default () => (
+  <Layout> 
+    <h1>Welcome to Hazza Alkaabi's web home &amp; playground</h1>
+    <p>You will find here his projects and writings</p>
   </Layout>
 )
-
-export default IndexPage
